c2ab0fcbb8d7fbd19de73aca893ec7a1e44b01eb
zgbbiao <1031516418@qq.com>
2018-06-20 18:16:11 +0800

    first -m

---
+++ web/myautuiweb/myweb/src/components/moblie.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="tMain mobile">
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "moblie"
+  }
+</script>
+<style scoped>
+  .mobile {
+    width: 360px;
+    height: 630px;
+    outline: none;
+    border: solid 20px #000;
+    border-radius: 40px;
+    -webkit-border-radius: 40px;
+    -moz-border-radius: 40px;
+    -o-border-radius: 40px;
+    -ms-border-radius: 40px;
+    border-bottom: solid 80px #000;
+    border-top: solid 60px #000;
+  }
+  .tMain {
+    position: relative;
+    overflow-y: auto;
+    overflow-x: hidden;
+    margin-right: 30px;
+  }
+</style>
---
+++ web/myautuiweb/myweb/src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Layout></Layout>
+  </div>
+</template>
+
+<script>
+  import Layout from "@/components/layout"
+
+  export default {
+  name: 'index',
+    components: {
+      'Layout': Layout
+    }
+}
+</script>
+
+<style>
+</style>
